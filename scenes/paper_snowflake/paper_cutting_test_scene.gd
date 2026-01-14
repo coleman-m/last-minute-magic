9d2e0ed1ba95c99f8d938c8844d9cc006f90acf1
Ashpaca <ashley.ashley.harrison@gmail.com>
2026-01-13 16:08:10 -0800

    making paper cutting simpler

--- scenes/paper_snowflake/paper_cutting_test_scene.gd
+++ scenes/paper_snowflake/paper_cutting_test_scene.gd
@@ -1,18 +1,33 @@
 extends Node2D
 
+@onready var polygon_2d: Polygon2D = $Polygon2D
+
+var polygon_points : Array[Vector2] = [Vector2(0,0),Vector2(1200,0),Vector2(sqrt(3) / 2 * 1200, -600)]
+var polygon_position : Vector2 = Vector2(350, 800)
 var previous_mouse_position : Vector2
 var mouse_positions : Array[Vector2]
+var polygons : Array[PackedVector2Array]
+
+# find which points are inside / outside the area, return the indexs of the points inside the triangle
+func sort_points_in() -> Array[int]:
+	return []
+
 
 func _draw():
 	if mouse_positions.size() < 2: return
 	if Input.is_action_pressed("mouse_down"):
 		draw_polyline(mouse_positions, Color("478cbf"), 10)
 	else:
-		draw_colored_polygon(mouse_positions, Color("478cbf"))
+		polygons.append_array(Geometry2D.merge_polygons(mouse_positions, PackedVector2Array([])))
+	if not polygons.is_empty():	
+		for poly in polygons:
+			draw_colored_polygon(poly, Color("478cbf"))
 
 func _ready() -> void:
 	previous_mouse_position = get_global_mouse_position()
-	
+	polygon_2d.polygon = polygon_points
+	polygon_2d.position = polygon_position
+
 
 func _process(_delta):
 	var current_mouse_position = get_global_mouse_position()
