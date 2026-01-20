611a1aa066b88a597c76b32dc5a137ec23956573
Ashpaca <ashley.ashley.harrison@gmail.com>
2026-01-19 22:21:32 -0800

    some more background sounds

--- scenes/main/main_camera.gd
+++ scenes/main/main_camera.gd
@@ -1,10 +1,26 @@
 extends Camera2D
 
+const AMBIENCE_SUBURBAN_NIGHT = preload("res://assets/sounds/Ambience_Suburban_Night by Eric Matyas www.soundimage.org.mp3")
+const WHIMSICAL_POPSICLE = preload("res://assets/sounds/Whimsical-Popsicle by Eric Matyas www.soundimage.org.ogg")
+
 @onready var screen_transition: Control = $ScreenTransition
+@onready var background_sfx: AudioStreamPlayer2D = $BackgroundSfx
 
 func _on_camera_move(new_position : Vector2, new_zoom : float) -> void:
 	global_position = new_position
 	zoom = Vector2.ONE * new_zoom
+
+
+func _on_state_change() -> void:
+	if GameState.current_state == GameState.State.MENU:
+		background_sfx.stream = AMBIENCE_SUBURBAN_NIGHT
+		background_sfx.play()
+	if GameState.current_state == GameState.State.SNOWFLAKE:
+		background_sfx.stream = WHIMSICAL_POPSICLE
+		background_sfx.play()
+	if GameState.current_state == GameState.State.TREE:
+		background_sfx.stream = WHIMSICAL_POPSICLE
+		background_sfx.play()
 
 
 func _process(_delta: float) -> void:
@@ -13,3 +29,4 @@ func _process(_delta: float) -> void:
 
 func _ready() -> void:
 	EventBus.move_camera.connect(_on_camera_move)
+	EventBus.state_changed.connect(_on_state_change)
