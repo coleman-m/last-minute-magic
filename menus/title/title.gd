a7494063254ed40216d0526a742f8db17fe337c0
Ashpaca <ashley.ashley.harrison@gmail.com>
2026-01-25 19:28:35 -0800

    changed the opening scene to make sense

--- menus/title/title.gd
+++ menus/title/title.gd
@@ -13,3 +13,16 @@ func _on_cutscene_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "start_scene":
 		visible = false
 		GameState.change_state(GameState.State.MENU)
+
+
+func _process(_delta: float) -> void:
+	if not cutscene_player.is_playing() or cutscene_player.current_animation == "title_screen": return
+	if Input.is_action_just_pressed("mouse_down"):
+		if cutscene_player.current_animation_position < 1.4667:
+			cutscene_player.play_section("start_scene", 1.4667)
+		elif cutscene_player.current_animation_position < 5.9667:
+			cutscene_player.play_section("start_scene", 5.9667)
+		elif cutscene_player.current_animation_position < 12.0667:
+			cutscene_player.play_section("start_scene", 12.0667)
+		elif cutscene_player.current_animation_position < 18.19:
+			cutscene_player.play_section("start_scene", 18.19)
